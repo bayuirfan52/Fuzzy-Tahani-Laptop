6a4608f470083ad49096842d855e310fcd2d2e76
bayuirfan52 <bayuirfan52@gmail.com>
2019-05-06 21:54:03 +0700

    Update for all UI Preparing

---
+++ application/views/main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="<?=base_url()?>/assets/css/bootstrap.min.css">
+	<script src="<?=base_url()?>/assets/js/jquery-3.4.1.min.js"></script>
+	<script src="<?=base_url()?>/assets/js/bootstrap.min.js"></script>
+	<title>Dashboard</title>
+</head>
+<body>
+	<div class="jumbotron text-center">
+		<h1>Software Pendukung Rekomendasi Laptop</h1>
+		<p>Aplikasi rekomendasi dengan menerapkan Fuzzy Database Tahani</p>
+	</div>
+	<div class="container">
+
+	</div>
+</body>
+</html>
